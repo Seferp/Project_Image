94411d48058a8aab75d89867b1b2dafb6f6c412b
P.Seferyniak <p.seferyniak@gmail.com>
2023-10-09 23:47:40 +0200

    Prepered views and serializers.

---
+++ project_image/imageApp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import UploadImageList
+from django.contrib.auth.views import LoginView, LogoutView
+
+
+urlpatterns = [
+    path('images/', UploadImageList.as_view(), name='image-list'),
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='registration/logged_out.html'), name='logout'),
+]

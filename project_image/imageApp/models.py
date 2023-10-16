d572ee01efc8977b60c7016ab62df4628cbf00af
P.Seferyniak <p.seferyniak@gmail.com>
2023-10-16 23:12:06 +0200

    Quick review

--- project_image/imageApp/models.py
+++ project_image/imageApp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
 from django.urls import reverse
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 # Create your models here.
